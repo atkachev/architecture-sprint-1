32fd065254942a22f59f206d2613bde20a1bc279
andreitkacev <tkachev87@gmail.com>
2024-10-13 00:23:38 +0400

    1 и 2 уровень

--- frontend/microfrontend/card/src/utils/api.js
+++ frontend/microfrontend/card/src/utils/api.js
@@ -12,8 +12,8 @@ class Api {
     return Promise.all([this.getCardList(), this.getUserInfo()]);
   }
 
-  getUserInfo() {
-    return fetch(`${this._address}/${this._groupId}/users/me`, {
+  getCardList() {
+    return fetch(`${this._address}/${this._groupId}/cards`, {
       headers: {
         authorization: this._token,
       },
@@ -21,35 +21,30 @@ class Api {
       .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
   }
 
-  setUserInfo({ name, about }) {
-    return fetch(`${this._address}/${this._groupId}/users/me`, {
-      method: 'PATCH',
+  addCard({ name, link }) {
+    return fetch(`${this._address}/${this._groupId}/cards`, {
+      method: 'POST',
       headers: {
         authorization: this._token,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         name,
-        about,
+        link,
       }),
     })
       .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
   }
 
-  setUserAvatar({ avatar }) {
-    return fetch(`${this._address}/${this._groupId}/users/me/avatar`, {
-      method: 'PATCH',
+  removeCard(cardID) {
+    return fetch(`${this._address}/${this._groupId}/cards/${cardID}`, {
+      method: 'DELETE',
       headers: {
         authorization: this._token,
-        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        avatar,
-      }),
     })
       .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
   }
-
 }
 
 const api = new Api({
